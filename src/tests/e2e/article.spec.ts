3d0c16609242e5716e7491d1f82f40d72983986a
Ernesto Albarez <ernestoalbrz@gmail.com>
2025-04-24 19:34:40 -0300

    add article test

---
+++ src/tests/e2e/article.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "../../fixtures/article.fixture";
+import { ArticlePage } from "../../pages/article.page";
+
+test.describe('LA NACION Article Page', () => {
+  test.use({
+    articleOptions: { 
+      articlePath: '/politica/tras-la-caida-de-la-moratoria-la-oposicion-desafia-al-gobierno-impulsa-una-jubilacion-proporcional-a-nid25032025/'
+    }
+  });
+
+  test('should display article title', async ({ articlePage }) => {
+    await articlePage.verifyArticleTitle();
+  });
+
+  test('should have a paragraph visble', async ({ articlePage }) => {
+    await articlePage.verifyArticleHasVisibleParagraphs();
+  });
+
+  test('should validate header', async({ articlePage }) => {
+    await articlePage.verifyHeader();
+  });
+
+  test('should validate footer', async({ articlePage }) => {
+    await articlePage.verifyFooter();
+  });
+});
