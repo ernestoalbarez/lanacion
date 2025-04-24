9b86154fca7ced488442c7617a8d4c315fcad3b0
Ernesto Albarez <ernestoalbrz@gmail.com>
2025-04-23 21:25:56 -0300

    update fixture file with methods for requests validation

--- src/fixtures/home.fixture.ts
+++ src/fixtures/home.fixture.ts
@@ -1,27 +1,49 @@
-import { HomePage } from'../pages/homePage';
-import { test as base } from "@playwright/test";
+import { HomePage } from '../pages/homePage';
+import { test as baseTest, expect } from "@playwright/test";
+
+type FailedRequest = {
+  url: string;
+  status: number;
+  method: string;
+  resource: string;
+};
 
 type HomeFixture = {
   homePage: HomePage;
+  collectFailedRequests: FailedRequest[];
 };
 
-export const test = base.extend<HomeFixture>({
+export const test = baseTest.extend<HomeFixture>({
   homePage: async ({ page }, use) => {
-    await use(new HomePage(page));
+    const homePage = new HomePage(page);
+    await homePage.navigate();
+    await use(homePage);
   },
-  page: async ({ page }, use) => {
-    try {
-      if (!page.isClosed()) {
-        await page.goto("/");
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.warn("Warning: page.goto('/') failed in fixture:", error.message);
-      } else {
-        console.warn("Warning: page.goto('/') failed in fixture with an unknown error.");
-      }
-    }
 
-    await use(page);
+  collectFailedRequests: async ({ page }, use) => {
+    const failedRequests: FailedRequest[] = [];
+    const targetDomain = 'lanacion.com.ar';
+
+    page.on('response', response => {
+      const url = response.url();
+      const status = response.status();
+      
+      if (url.includes(targetDomain) && !/^2/.test(status.toString())) {
+        failedRequests.push({
+          url,
+          status,
+          method: response.request().method(),
+          resource: response.request().resourceType()
+        });
+      }
+    });
+
+    await use(failedRequests);
   },
+
+  page: async ({ page }, use) => {
+    await use(page);
+  }
 });
+
+export { expect };
